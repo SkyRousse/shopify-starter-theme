1c5d48536dd21a67e1ba749d712c2d6f1db12946
Sky Rousse <skyrousse@gmail.com>
2023-01-07 15:57:59 -0800

    add missing snippets and update file names

--- assets/scripts.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  // source/scripts/global.js
-  console.log("Hi from global.js!");
-})();
-//# sourceMappingURL=scripts.js.map
--- build/entryPoints.js
+++ build/entryPoints.js
@@ -1,4 +1,4 @@
 module.exports.entryPoints = {
-  scripts: './source/scripts/global.js',
-  style: './source/styles/base/base.css',
+  global: './source/scripts/global.js',
+  base: './source/styles/base/base.css',
 };
